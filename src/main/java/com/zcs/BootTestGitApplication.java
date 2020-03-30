600a321b9a8f7da40bb76cfe17762cf67945bb36
project_manager <1529994247@qq.com>
2020-03-30 23:11:18 +0800

    项目经理第一次提交

---
+++ src/main/java/com/zcs/BootTestGitApplication.java
@@ -0,0 +1,13 @@
+package com.zcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTestGitApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zcs/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.zcs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zcs
+ * @data: 2020/3/30
+ * @description:
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello git";
+    }
+}
